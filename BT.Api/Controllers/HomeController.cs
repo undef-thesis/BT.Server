99cb7aebd14b54b0bb52621335b2ee92e41be95d
szatkoaleksander <szatko.aleksander@gmail.com>
2020-08-10 11:36:31 +0200

    Initial commit

---
+++ BT.Api/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BT.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+    
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "BT.Server works!";
+        }
+    }
+}
