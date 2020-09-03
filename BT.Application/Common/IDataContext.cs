15363a237ab5c8841ebbb33bba11966d88e97bed
szatkoaleksander <szatko.aleksander@gmail.com>
2020-09-03 21:44:13 +0200

    Update relationship

--- BT.Application/Common/IDataContext.cs
+++ BT.Application/Common/IDataContext.cs
@@ -8,6 +8,9 @@ namespace BT.Application.Common
     {
         DbSet<User> Users { get; set; }
         DbSet<RefreshToken> RefreshToken { get; set; }
+        DbSet<Meeting> Meetings { get; set; }
+        DbSet<UserMeeting> UserMeeting { get; set; }
+        DbSet<Address> Address { get; set; }
         Task<int> SaveChangesAsync();
     }
 }
